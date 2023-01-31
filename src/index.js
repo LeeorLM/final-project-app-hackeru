6747e849bd1ab613b581aa09d41aa07753a7e887
LeeorLM <100274447+LeeorLM@users.noreply.github.com>
2023-01-31 15:35:36 +0200

    Revert "finals"
    This reverts commit 57da686d497d5476a0da7684b9dfcb6eb62dd800.

--- src/index.js
+++ src/index.js
@@ -1,44 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-// import "./index.scss";
-import "./css/index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import axios from "axios";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-/* import bootstrap to react project */
-// import "../node_modules/bootstrap/dist/css/bootstrap.css";
-// import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
-/*toastify */
-import "react-toastify/dist/ReactToastify.css";
-/* redux */
-import { Provider } from "react-redux";
-import store from "./store/index";
-/* React router dom */
-import { BrowserRouter } from "react-router-dom";
-
-/*axios configuration */
-// add this url before every axios request
-// if the url is relative - then axios will ignore this url
-axios.defaults.baseURL = process.env.REACT_APP_BASE_API_URL;
-
-axios.interceptors.request.use((config) => {
-  const finalProjectToken = localStorage.getItem("finalProjectToken");
-  if (finalProjectToken) {
-    // if token saved in local storage then i want to add the token to the header of the request
-    config.headers["x-auth-token"] = finalProjectToken;
-  }
-  return config;
-});
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <App />
   </React.StrictMode>
 );
 
